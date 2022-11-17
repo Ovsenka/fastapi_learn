69ebccf0513033edfc25a8c9f153483ecbfa499a
Ovsenka <ovsenka3@gmail.com>
2022-11-17 20:06:21 +0300

    path parameters

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
+class UserNames(str, Enum):
+    one = "John"
+    two = "Max"
+    three = "Jack" 
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello,world!"}
+
+@app.get("/get/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/user/{user}")
+async def get_user(user: UserNames):
+    print(user.value, user)
+    return {"user": user}
